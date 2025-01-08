6d941ef6920c627f85dce8cb720ca18f66aaa46e
vinayjanjirala <vinayjanjirala7@gmail.com>
2025-01-08 21:28:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+//first task
+var ul=document.createElement("ul");
+var li1=document.createElement("li");
+li1.innerText="one";
+var li2=document.createElement("li");
+li2.innerText="two";
+var li3=document.createElement("li");
+li3.innerText="three";
+var li4=document.createElement("li");
+li4.innerText="four";
+var button5=document.createElement("li");
+button5.innerText="five";
+ul.append(li1,li2,li3,li4,button5)
+document.body.appendChild(ul);
+//second task
+var div1=document.createElement("div");
+var p1=document.createElement("p");
+p1.innerText="paragraph";
+div1.append(p1)
+document.body.appendChild(div1)
+//third task
+var div2=document.createElement("div");
+var h1=document.createElement("h1");
+h1.innerText="Heading";
+var p2=document.createElement("p");
+p2.innerText="paragraph2";
+div2.append(h1,p2)
+document.body.appendChild(div2)
+//fourth task
+var table=document.createElement("table");
+var tr=document.createElement("tr");
+var td1=document.createElement("td");
+td1.innerText="one";
+var td2=document.createElement("td");
+td2.innerText="two";
+var td3=document.createElement("td");
+td3.innerText="three";
+tr.append(td1,td2,td3)
+table.append(tr)
+document.body.appendChild(table);
+//fifth task
+var div3=document.createElement("div");
+var button1=document.createElement("button");
+button1.innerText="one";
+var button2=document.createElement("button");
+button2.innerText="two";
+var button3=document.createElement("button");
+button3.innerText="three";
+var button4=document.createElement("button");
+button4.innerText="four";
+var button5=document.createElement("button");
+button5.innerText="five";
+div3.append(button1,button2,button3,button4,button5);
+document.body.appendChild(div3)
+
+
+
+
